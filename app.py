e106dfb7c2c6094c9e978871e0953e1d95657225
Ankur Limbashia <ankurlimbashia12@gmail.com>
2024-08-16 16:39:05 -0400

    added comment

--- app.py
+++ app.py
@@ -33,12 +33,13 @@ def get_config_value(key):
 
 def retrieve_data(ticker):
     """
-    returns the data from API if API responds to the request
+    returns the data from API if API responds to the request otherwise it will return the data from json files
     """
 
     url = get_config_value('asx_announcements_url') % ticker.upper()
 
     response = requests.get(url,headers=get_config_value('headers'))
+
 
     try:
         return response.json()['data']
@@ -51,14 +52,17 @@ def retrieve_data(ticker):
 
 
 def main():
-
+    # get the tickers
     tickers = sorted(get_config_value('tickers'))
+
+    # setting up the page
     st.set_page_config(page_title = 'ASX API Data Retrieval', 
                 page_icon='📰'
                 )
     
     st.title('ASX Announcements')
 
+    # radio button to switch between two tasks
     task_selected = st.radio('',['Announcements','Trading Halt Tickers'])
     
     if task_selected == 'Announcements':
@@ -66,7 +70,8 @@ def main():
         selected_ticker = st.selectbox("Tickers",tickers)
         data = retrieve_data(selected_ticker)
         
-        if data is not None: # check the request response
+        if data is not None:
+            # dictionary to store the data
             announcements = {'title':[],'url':[],'document_release_date':[]}
             for d in data:
                 announcements['title'].append(d['header'])
@@ -74,21 +79,26 @@ def main():
                 announcements['document_release_date'].append(d['document_release_date'])
             
             announcements = pd.DataFrame(announcements)
+
+            # new column to have headers with hyperlink
             announcements[data[0]['issuer_full_name']] = announcements.apply(lambda row: f'<a href="{row["url"]}" target="_blank">{row["title"]}</a>', axis=1)
             announcements['document_release_date'] = pd.to_datetime(announcements['document_release_date']) 
+            
+            # printing the 20 announcements
             st.markdown(announcements[[data[0]['issuer_full_name'],'document_release_date']].to_html(escape=False, index=False), unsafe_allow_html=True)
     
     else:
         tickers_with_trading_halt = {'ticker':[],'ticker_full_name':[]}
         for ticker in tickers:
             data = retrieve_data(ticker)
-            if data is None: # check the request response
+            if data is None:
                 break
             for d in data:
                 if 'Trading Halt'.lower() in d['header'].lower():
                     tickers_with_trading_halt['ticker'].append(ticker)
                     tickers_with_trading_halt['ticker_full_name'].append(d['issuer_full_name'])
                     break
+        # printing the tickers with trading halt
         st.write(pd.DataFrame(tickers_with_trading_halt,columns=['Tickers','Ticker\'s Full Name']))
 
 
